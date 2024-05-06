142cc544d56708ca4802dd50d6dbac938f2a2d0a
aCrazyg1 <huxiaodou768@hotmail.com>
2024-05-06 18:53:49 -0400

    start and end methods plus registerring suicide command and the onjoinleave listener, java level set to lvl16

--- src/main/java/me/crazyg/everything/Everything.java
+++ src/main/java/me/crazyg/everything/Everything.java
@@ -1,5 +1,9 @@
 package me.crazyg.everything;
 
+import me.crazyg.everything.commands.SuicideCommand;
+import me.crazyg.everything.listeners.onJoinleaveListener;
+import org.bukkit.event.Listener;
+import org.bukkit.plugin.Plugin;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public final class Everything extends JavaPlugin {
@@ -7,11 +11,25 @@ public final class Everything extends JavaPlugin {
     @Override
     public void onEnable() {
         // Plugin startup logic
+        System.out.println("THIS PLUGIN IS WRITTENED BY CRAZYG");
+        System.out.println("THANKS FOR USING THE PLUGIN");
+        System.out.println("\n" +
+                "███████╗██╗░░░██╗███████╗██████╗░██╗░░░██╗████████╗██╗░░██╗██╗███╗░░██╗░██████╗░\n" +
+                "██╔════╝██║░░░██║██╔════╝██╔══██╗╚██╗░██╔╝╚══██╔══╝██║░░██║██║████╗░██║██╔════╝░\n" +
+                "█████╗░░╚██╗░██╔╝█████╗░░██████╔╝░╚████╔╝░░░░██║░░░███████║██║██╔██╗██║██║░░██╗░\n" +
+                "██╔══╝░░░╚████╔╝░██╔══╝░░██╔══██╗░░╚██╔╝░░░░░██║░░░██╔══██║██║██║╚████║██║░░╚██╗\n" +
+                "███████╗░░╚██╔╝░░███████╗██║░░██║░░░██║░░░░░░██║░░░██║░░██║██║██║░╚███║╚██████╔╝\n" +
+                "╚══════╝░░░╚═╝░░░╚══════╝╚═╝░░╚═╝░░░╚═╝░░░░░░╚═╝░░░╚═╝░░╚═╝╚═╝╚═╝░░╚══╝░╚═════╝░");
+        // Commands
+        getCommand("suicide").setExecutor(new SuicideCommand());
+        // Listeners
+        getServer().getPluginManager().registerEvents((Listener) new onJoinleaveListener(), (Plugin) this);
 
     }
 
     @Override
     public void onDisable() {
         // Plugin shutdown logic
+        System.out.println("CYA LATER!!!!!");
     }
 }
