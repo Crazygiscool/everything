828cdc3a6e14ccbdea826aa181873de3fe0ca9b0
aCrazyg1 <huxiaodou768@hotmail.com>
2024-05-06 21:43:28 -0400

    updated the god command to add a argument to target another player thats online

--- src/main/java/me/crazyg/everything/commands/GodCommand.java
+++ src/main/java/me/crazyg/everything/commands/GodCommand.java
@@ -1,5 +1,6 @@
 package me.crazyg.everything.commands;
 
+import org.bukkit.Bukkit;
 import org.bukkit.ChatColor;
 import org.bukkit.command.*;
 import org.bukkit.entity.Player;
@@ -8,15 +9,39 @@ import org.bukkit.entity.Player;
 public class GodCommand implements CommandExecutor{
 
     @Override
-    public boolean onCommand(CommandSender sender, Command command, String label, String[] strings) {
+    public boolean onCommand(CommandSender sender, Command command, String label, String[] args) {
 
         if(sender instanceof Player p){
-            if(p.isInvulnerable()){
-                p.setInvulnerable(false);
-                p.sendMessage(ChatColor.DARK_RED+"GOD MODE disabled");
+            if (args.length == 0){
+                if(p.isInvulnerable()){
+                    p.setInvulnerable(false);
+                    p.sendMessage(ChatColor.DARK_RED+"GOD MODE disabled");
+                }else{
+                    p.setInvulnerable(true);
+                    p.sendMessage(ChatColor.GOLD+"GOD MODE Enabled");
+                }
             }else{
-                p.setInvulnerable(true);
-                p.sendMessage(ChatColor.GOLD+"GOD MODE Enabled");
+
+                //set the string playername to the first argument
+                String playername = args[0];
+
+                //get the target player name to store in to the target argument
+                Player target = Bukkit.getServer().getPlayerExact(playername);
+
+                if(target == null){
+                    p.sendMessage("This Player is not online");
+                }else{
+                    if(p.isInvulnerable()){
+                        target.setInvulnerable(false);
+                        target.sendMessage(ChatColor.DARK_RED+"GOD MODE disabled by "+p.getDisplayName());
+                        p.sendMessage(ChatColor.BLUE+"MADE HIM UNGOD");
+                    }else {
+                        target.setInvulnerable(true);
+                        target.sendMessage(ChatColor.GOLD + "GOD MODE Enabled by "+p.getDisplayName());
+                        p.sendMessage(ChatColor.BLUE+"MADE HIM GOD");
+                    }
+                }
+
             }
         }else if (sender instanceof ConsoleCommandSender p) {
             p.sendMessage(ChatColor.DARK_RED+""+ChatColor.BOLD+"Command Cannot be runned by console");
