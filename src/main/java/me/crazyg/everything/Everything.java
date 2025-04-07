6cb21060014a4e4fc99354b4ab271c7fad66b64d
crazygiscool <huxiaodou768@gmail.com>
2025-04-07 16:37:09 -0400

    removed menu commands because it aint working, added some gamemodes commands similar to essentials

--- src/main/java/me/crazyg/everything/Everything.java
+++ src/main/java/me/crazyg/everything/Everything.java
@@ -24,6 +24,7 @@ public final class Everything extends JavaPlugin {
         getCommand("suicide").setExecutor(new KillCommand());
         getCommand("god").setExecutor(new GodCommand());
         getCommand("repeat").setExecutor(new RepeatCommand());
+        getCommand("gmc").setExecutor(new GmcCommand());
         // Listeners
         getServer().getPluginManager().registerEvents((Listener) new onJoinleaveListener(this), (Plugin) this);
         //config.yml
---
+++ src/main/java/me/crazyg/everything/commands/GmsCommand.java
@@ -0,0 +1,47 @@
+package me.crazyg.everything.commands;
+
+import org.bukkit.Bukkit;
+import org.bukkit.ChatColor;
+import org.bukkit.GameMode;
+import org.bukkit.command.*;
+import org.bukkit.entity.Player;
+
+
+public class GmsCommand implements CommandExecutor{
+
+    @Override
+    public boolean onCommand(CommandSender sender, Command command, String label, String[] args) {
+
+        if (sender instanceof Player p) {
+            if (args.length == 0) {
+                if (p.getGameMode() == GameMode.SURVIVAL) {
+                    p.setGameMode(GameMode.SURVIVAL);
+                    p.sendMessage(ChatColor.DARK_RED + "Gamemode set to" + ChatColor.GOLD + ChatColor.BOLD + "Survival Mode");
+                } else {
+
+                    //set the string playername to the first argument
+                    String playername = args[0];
+
+                    //get the target player name to store in to the target argument
+                    Player target = Bukkit.getServer().getPlayerExact(playername);
+
+                    if (target == null) {
+                        p.sendMessage("This Player is not online");
+                    } else {
+                        if (p.isInvulnerable()) {
+                            target.setGameMode(GameMode.SURVIVAL);
+                            target.sendMessage(ChatColor.DARK_RED + "Set to " + ChatColor.GOLD + ChatColor.BOLD + "Survival Mode " + ChatColor.DARK_RED + "by " + ChatColor.DARK_AQUA + ChatColor.BOLD + p.getDisplayName());
+                            p.sendMessage(ChatColor.BLUE + "Set " + ChatColor.DARK_AQUA + ChatColor.BOLD + p.getDisplayName() + ChatColor.BLUE + "to " + ChatColor.GOLD + ChatColor.BOLD + "Survival Mode");
+                        }
+
+                    }
+                }
+            } else if (sender instanceof ConsoleCommandSender m) {
+                m.sendMessage(ChatColor.DARK_RED + "" + ChatColor.BOLD + "Command Cannot be runned by console");
+            } else if (sender instanceof BlockCommandSender m) {
+                m.sendMessage(ChatColor.DARK_RED + "" + ChatColor.BOLD + "Command Cannot be runned by command block");
+            }
+        }
+        return true;
+    }
+}
