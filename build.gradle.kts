57820cc21436f773722f21598d2056a35714ab22
Crazygiscool <crazygiscool@proton.me>
2025-12-12 22:25:21 -0500

    added gradle files and setting to began the transition. Added .gradle and build folder to .gitignore

---
+++ build.gradle.kts
@@ -0,0 +1,42 @@
+plugins {
+    java
+    id("com.github.johnrengelman.shadow") version "8.1.1"
+}
+
+group = "me.crazyg"
+version = "1.2.0"
+
+java {
+    toolchain.languageVersion.set(JavaLanguageVersion.of(17))
+}
+
+repositories {
+    mavenCentral()
+    maven("https://repo.papermc.io/repository/maven-public/")
+    maven("https://oss.sonatype.org/content/groups/public/")
+    maven("https://repo.helpch.at/releases")
+    maven("https://jitpack.io")
+}
+
+dependencies {
+    compileOnly("io.papermc.paper:paper-api:1.20.4-R0.1-SNAPSHOT")
+    compileOnly("me.clip:placeholderapi:2.11.5")
+    compileOnly("com.github.MilkBowl:VaultAPI:1.7.1")
+    implementation("com.google.code.gson:gson:2.10.1")
+}
+
+tasks.withType<JavaCompile> {
+    options.encoding = "UTF-8"
+}
+
+tasks.processResources {
+    filteringCharset = "UTF-8"
+    filesMatching("plugin.yml") {
+        expand(project.properties)
+    }
+}
+
+tasks.shadowJar {
+    archiveBaseName.set("everything")
+    archiveClassifier.set("")
+}
